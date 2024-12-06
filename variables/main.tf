0a7f1c7a065498489863d460fa5a469cf943e921
Vlad Voloshyn <vladreamer@gmail.com>
2024-12-06 16:01:45 -0500

    updatei #3

---
+++ variables/main.tf
@@ -0,0 +1,31 @@
+variable "string_var" {
+  type = string
+  default = "default string variable"
+}
+
+variable "number_var" {
+    type = number
+    default = 100
+}
+
+variable "bool_var" {
+   default = false  
+}
+
+variable "list_vars" {
+    type = list(string)
+    default = [ "apple", "peach" ]
+}
+
+variable "list_vars" {
+    type = list(number)
+    default = [ 10, 20 ]
+}
+
+variable "map_vars" {
+    type = map
+    default = {
+        key1 = "value1"
+        key2 = "value2"
+    }
+}
